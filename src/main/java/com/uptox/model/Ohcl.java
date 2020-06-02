67fa3febf2571145746d4c6ad2d5608e4e68f58a
ersansingh <ersansingh1984@gmail.com>
2020-06-02 14:30:30 +0530

    delombok model classes

--- src/main/java/com/uptox/model/Ohcl.java
+++ src/main/java/com/uptox/model/Ohcl.java
@@ -1,11 +1,5 @@
 package com.uptox.model;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
-
-@Builder(toBuilder = true)
-@ToString @Getter
 public class Ohcl {
     private String event;
     private String symbol;
@@ -15,4 +9,121 @@ public class Ohcl {
     private double low;
     private double close;
     private double volume;
+
+    Ohcl(String event, String symbol, int bar_num, double open, double high, double low, double close, double volume) {
+        this.event = event;
+        this.symbol = symbol;
+        this.bar_num = bar_num;
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.close = close;
+        this.volume = volume;
+    }
+
+    public static OhclBuilder builder() {
+        return new OhclBuilder();
+    }
+
+    public String getEvent() {
+        return this.event;
+    }
+
+    public String getSymbol() {
+        return this.symbol;
+    }
+
+    public int getBar_num() {
+        return this.bar_num;
+    }
+
+    public double getOpen() {
+        return this.open;
+    }
+
+    public double getHigh() {
+        return this.high;
+    }
+
+    public double getLow() {
+        return this.low;
+    }
+
+    public double getClose() {
+        return this.close;
+    }
+
+    public double getVolume() {
+        return this.volume;
+    }
+
+    public String toString() {
+        return "Ohcl(event=" + this.getEvent() + ", symbol=" + this.getSymbol() + ", bar_num=" + this.getBar_num() + ", open=" + this.getOpen() + ", high=" + this.getHigh() + ", low=" + this.getLow() + ", close=" + this.getClose() + ", volume=" + this.getVolume() + ")";
+    }
+
+    public OhclBuilder toBuilder() {
+        return new OhclBuilder().event(this.event).symbol(this.symbol).bar_num(this.bar_num).open(this.open).high(this.high).low(this.low).close(this.close).volume(this.volume);
+    }
+
+    public static class OhclBuilder {
+        private String event;
+        private String symbol;
+        private int bar_num;
+        private double open;
+        private double high;
+        private double low;
+        private double close;
+        private double volume;
+
+        OhclBuilder() {
+        }
+
+        public Ohcl.OhclBuilder event(String event) {
+            this.event = event;
+            return this;
+        }
+
+        public Ohcl.OhclBuilder symbol(String symbol) {
+            this.symbol = symbol;
+            return this;
+        }
+
+        public Ohcl.OhclBuilder bar_num(int bar_num) {
+            this.bar_num = bar_num;
+            return this;
+        }
+
+        public Ohcl.OhclBuilder open(double open) {
+            this.open = open;
+            return this;
+        }
+
+        public Ohcl.OhclBuilder high(double high) {
+            this.high = high;
+            return this;
+        }
+
+        public Ohcl.OhclBuilder low(double low) {
+            this.low = low;
+            return this;
+        }
+
+        public Ohcl.OhclBuilder close(double close) {
+            this.close = close;
+            return this;
+        }
+
+        public Ohcl.OhclBuilder volume(double volume) {
+            this.volume = volume;
+            return this;
+        }
+
+        public Ohcl build() {
+            return new Ohcl(event, symbol, bar_num, open, high, low, close, volume);
+        }
+
+        public String toString() {
+            return "Ohcl.OhclBuilder(event=" + this.event + ", symbol=" + this.symbol + ", bar_num=" + this.bar_num + ", open=" + this.open + ", high=" + this.high + ", low=" + this.low + ", close=" + this.close + ", volume=" + this.volume + ")";
+        }
+    }
 }
--- src/main/java/com/uptox/model/Stock.java
+++ src/main/java/com/uptox/model/Stock.java
@@ -1,13 +1,7 @@
 package com.uptox.model;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
-
 import java.time.ZonedDateTime;
 
-@Getter @ToString
-@Builder(toBuilder = true)
 public class Stock {
     private String sym;
     private String T;
@@ -15,4 +9,99 @@ public class Stock {
     private double Q;
     private String side;
     private ZonedDateTime TS2;
+
+    Stock(String sym, String T, double P, double Q, String side, ZonedDateTime TS2) {
+        this.sym = sym;
+        this.T = T;
+        this.P = P;
+        this.Q = Q;
+        this.side = side;
+        this.TS2 = TS2;
+    }
+
+    public static StockBuilder builder() {
+        return new StockBuilder();
+    }
+
+    public String getSym() {
+        return this.sym;
+    }
+
+    public String getT() {
+        return this.T;
+    }
+
+    public double getP() {
+        return this.P;
+    }
+
+    public double getQ() {
+        return this.Q;
+    }
+
+    public String getSide() {
+        return this.side;
+    }
+
+    public ZonedDateTime getTS2() {
+        return this.TS2;
+    }
+
+    public String toString() {
+        return "Stock(sym=" + this.getSym() + ", T=" + this.getT() + ", P=" + this.getP() + ", Q=" + this.getQ() + ", side=" + this.getSide() + ", TS2=" + this.getTS2() + ")";
+    }
+
+    public StockBuilder toBuilder() {
+        return new StockBuilder().sym(this.sym).T(this.T).P(this.P).Q(this.Q).side(this.side).TS2(this.TS2);
+    }
+
+    public static class StockBuilder {
+        private String sym;
+        private String T;
+        private double P;
+        private double Q;
+        private String side;
+        private ZonedDateTime TS2;
+
+        StockBuilder() {
+        }
+
+        public Stock.StockBuilder sym(String sym) {
+            this.sym = sym;
+            return this;
+        }
+
+        public Stock.StockBuilder T(String T) {
+            this.T = T;
+            return this;
+        }
+
+        public Stock.StockBuilder P(double P) {
+            this.P = P;
+            return this;
+        }
+
+        public Stock.StockBuilder Q(double Q) {
+            this.Q = Q;
+            return this;
+        }
+
+        public Stock.StockBuilder side(String side) {
+            this.side = side;
+            return this;
+        }
+
+        public Stock.StockBuilder TS2(ZonedDateTime TS2) {
+            this.TS2 = TS2;
+            return this;
+        }
+
+        public Stock build() {
+            return new Stock(sym, T, P, Q, side, TS2);
+        }
+
+        public String toString() {
+            return "Stock.StockBuilder(sym=" + this.sym + ", T=" + this.T + ", P=" + this.P + ", Q=" + this.Q + ", side=" + this.side + ", TS2=" + this.TS2 + ")";
+        }
+    }
 }
